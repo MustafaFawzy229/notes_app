50a774089cbacb75544e10e37572ba3b70df0256
Mustafa Fawzy <hamouda777744@gmail.com>
2023-06-21 01:50:37 +0300

    solve keyboard problem in bottom sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 
 import 'add_note_form.dart';
@@ -28,8 +27,15 @@ class AddNoteButtonSheet extends StatelessWidget {
           builder: (context, state) {
             return AbsorbPointer(
               absorbing: state is AddNoteLoading ? true : false,
-              child: const SingleChildScrollView(
-                child: AddNoteForm(),
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: 16,
+                  right: 16,
+                  bottom: MediaQuery.of(context).viewInsets.bottom,
+                ),
+                child: const SingleChildScrollView(
+                  child: AddNoteForm(),
+                ),
               ),
             );
           },
